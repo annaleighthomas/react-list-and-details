2e41648fd6b0931f058e0c196472676cb2250478
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-28 13:26:32 -0400

    added link to detail page

--- src/components/app/characters/Character.jsx
+++ src/components/app/characters/Character.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Character = ({ name, species, status, image }) => (
+const Character = ({ name, species, status, image, character }) => (
   <figure>
-    <img src={image} alt={name} />
+    <Link to={`/character/${character.id}`}>
+      <img src={image} alt={name} /></Link>
     <figcaption>
       <p>{name}</p>
-      <p>{species}</p>
-      <p>{status}</p>
+      {/* <p>{species}</p>
+      <p>{status}</p> */}
     </figcaption>
-  </figure>
+  </figure >
 );
 
 Character.propTypes = {
   name: PropTypes.string.isRequired,
   species: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired
+  image: PropTypes.string.isRequired,
+  character: PropTypes.string.isRequired
 };
 
 export default Character;
